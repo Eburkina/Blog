6ae2ac87f1e2ece12bea967361481e67ba92b15c
Eburkina <smeburkina@gmail.com>
2021-05-25 11:02:56 +0000

    gestion des routes

--- src/routes/web.php
+++ src/routes/web.php
@@ -13,16 +13,16 @@ Route::get('actualites/search', [BlogController::class, 'search'])->name('search
 
 
 
-Route::get('actualite/list', [ActualiteController::class, 'index'])->middleware(['auth'])->name('actualite-liste');
-Route::get('actualite/add', [ActualiteController::class, 'create'])->middleware(['auth'])->name('actualite-add');
-Route::post('actualite/store', [ActualiteController::class, 'store'])->middleware(['auth'])->name('actualite-store');
-Route::get('actualite/edit/{uuid}', [ActualiteController::class, 'edit'])->middleware(['auth'])->name('actualite-edit');
-Route::put('actualite/update/{uuid}', [ActualiteController::class, 'update'])->middleware(['auth'])->name('actualite-update');
-Route::delete('actualite/delete/{uuid}', [ActualiteController::class, 'delete'])->middleware(['auth'])->name('actualite-delete');
+Route::get('dashboard/actualite/list', [ActualiteController::class, 'index'])->middleware(['auth'])->name('actualite-liste');
+Route::get('dashboard/actualite/add', [ActualiteController::class, 'create'])->middleware(['auth'])->name('actualite-add');
+Route::post('dashboard/actualite/store', [ActualiteController::class, 'store'])->middleware(['auth'])->name('actualite-store');
+Route::get('dashboard/actualite/edit/{uuid}', [ActualiteController::class, 'edit'])->middleware(['auth'])->name('actualite-edit');
+Route::put('dashboard/actualite/update/{uuid}', [ActualiteController::class, 'update'])->middleware(['auth'])->name('actualite-update');
+Route::delete('dashboard/actualite/delete/{uuid}', [ActualiteController::class, 'delete'])->middleware(['auth'])->name('actualite-delete');
 
-Route::get('categorie/list', [CategorieController::class, 'index'])->middleware(['auth'])->name('categorie-list');
-Route::get('categorie/add', [CategorieController::class, 'create'])->middleware(['auth'])->name('categorie-add');
-Route::post('categorie/store', [CategorieController::class, 'store'])->middleware(['auth'])->name('categorie-store');
-Route::get('categorie/edit/{uuid}', [CategorieController::class, 'edit'])->middleware(['auth'])->name('categorie-edit');
-Route::put('categorie/update/{uuid}', [CategorieController::class, 'update'])->middleware(['auth'])->name('categorie-update');
-Route::delete('categorie/delete/{uuid}', [CategorieController::class, 'delete'])->middleware(['auth'])->name('categorie-delete');
+Route::get('dashboard/categorie/list', [CategorieController::class, 'index'])->middleware(['auth'])->name('categorie-list');
+Route::get('dashboard/categorie/add', [CategorieController::class, 'create'])->middleware(['auth'])->name('categorie-add');
+Route::post('dashboard/categorie/store', [CategorieController::class, 'store'])->middleware(['auth'])->name('categorie-store');
+Route::get('dashboard/categorie/edit/{uuid}', [CategorieController::class, 'edit'])->middleware(['auth'])->name('categorie-edit');
+Route::put('dashboard/categorie/update/{uuid}', [CategorieController::class, 'update'])->middleware(['auth'])->name('categorie-update');
+Route::delete('dashboard/categorie/delete/{uuid}', [CategorieController::class, 'delete'])->middleware(['auth'])->name('categorie-delete');
