ec32fdc0702618b43b8d5a35f2e16df3a7c791d4
Eburkina <smeburkina@gmail.com>
2021-05-26 13:52:50 +0000

    package v1.0

--- src/views/pages/frontend/actualites/show.blade.php
+++ src/views/pages/frontend/actualites/show.blade.php
@@ -104,12 +104,12 @@ setlocale(LC_TIME, "fr_FR", "French");
               </div>
             </div>
    <!-- Side Widget -->
-            <div class="card my-4">
+            {{-- <div class="card my-4">
               <h5 class="card-header">banner</h5>
               <div class="card-body">
                 <img class="card-img-top" src="https://2.bp.blogspot.com/-vvG5hMTFOro/W6RaoxdAikI/AAAAAAAAK1k/jezYdP7fvfYvt15Jv8a0agrGQE2lMU8YgCKgBGAs/s1600/MASAI-2.jpg" alt="Card image cap">
               </div>
-            </div>
+            </div> --}}
             <!-- tweeter -->
     
           </div>
